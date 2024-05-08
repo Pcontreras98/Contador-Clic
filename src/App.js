45c317e37d7205e35dd10b261437e90618185572
Pcontreras98 <martin_contrerasn@hotmail.com>
2024-05-07 23:48:48 -0500

    cambios general

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import Boton from './componentes/Boton';
+import Contador from './componentes/Contador';
+import CodeLogo from './imagenes/freecodecamp-logo.png'
 
 function App() {
+
+  const manejarClic = () =>{
+    console.log('click');
+  }
+
+  const reiniciarContador = () =>{
+    console.log('Reiniciar');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='logo-contenedor'>
+        <img
+          className='logo'
+          src={CodeLogo}
+          alt='Logo'
+        />
+      </div>
+      <div className='contenedor-principal'>
+        <Contador
+          numeroClics={5}
+        />
+        <Boton
+          texto='Clic'
+          esBotonDeClic={true}
+          manejarClic={manejarClic}
+        />
+        <Boton
+          texto='Reiniciar'
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador}
+        />
+      </div>
+
     </div>
   );
 }
---
+++ src/componentes/Contador.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Contador( {numeroClics} ) {
+  return (
+    <div className='contador'>
+      {numeroClics}
+    </div>
+  );
+}
+
+export default Contador
